0f7d669e65d49f2c070c926b4048e24f17c34ff9
Félix Girault <felix.girault@gmail.com>
2013-07-15 22:09:52 +0200

    Added an interface for Doctrine caches

---
+++ lib/Essence/Cache/Engine/Doctrine.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ *	@author Félix Girault <felix.girault@gmail.com>
+ *	@license FreeBSD License (http://opensource.org/licenses/BSD-2-Clause)
+ */
+
+namespace Essence\Cache\Engine;
+
+use Essence\Cache\Engine;
+use Doctrine\Common\Cache\Cache;
+
+
+
+/**
+ *	A bridge to use a Doctrine cache.
+ *
+ *	@package fg.Essence.Cache.Engine
+ */
+
+class Doctrine implements Engine {
+
+	/**
+	 *	The Doctrine cache instance.
+	 *
+	 *	@var Doctrine\Common\Cache
+	 */
+
+	protected $_Cache = null;
+
+
+
+	/**
+	 *	The lifeTime to be passed to the save( ) method.
+	 *
+	 *	@var int
+	 */
+
+	protected $_ttl = 0;
+
+
+
+	/**
+	 *	Constructor.
+	 *
+	 *	@param Doctrine\Common\Cache $Cache The Doctrine cache to use.
+	 *	@param int $ttl The lifeTime to be passed to the save( ) method.
+	 */
+
+	public function __construct( Cache $Cache, $ttl = 0 ) {
+
+		$this->_Cache = $Cache;
+		$this->_ttl = $ttl;
+	}
+
+
+
+	/**
+	 *	{@inheritDoc}
+	 */
+
+	public function has( $key ) {
+
+		return $this->_Cache->contains( $key );
+	}
+
+
+
+	/**
+	 *	{@inheritDoc}
+	 */
+
+	public function get( $key, $default = null ) {
+
+		return $this->_Cache->fetch( $key ) ?: $default;
+	}
+
+
+
+	/**
+	 *	{@inheritDoc}
+	 */
+
+	public function set( $key, $data ) {
+
+		$this->_Cache->save( $key, $data, $this->_ttl );
+		return $data;
+	}
+
+
+
+	/**
+	 *	{@inheritDoc}
+	 */
+
+	public function clear( ) { }
+
+}
